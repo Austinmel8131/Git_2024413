bc162a220b40154d7dcdf809379881fe3e12ad28
Austinmel8131 <117985076+Austinmel8131@users.noreply.github.com>
2024-12-02 12:05:46 +0000

    1/12/2024 password checker program

---
+++ src/main/java/com/mycompany/git_2024413/Git_2024413.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.git_2024413;
+
+/**
+ *
+ * @author macbook
+ */
+public class Git_2024413 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
